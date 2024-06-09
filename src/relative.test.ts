a12929947cfae5a72216516840e210f2c490508d
linyows <linyows@gmail.com>
2024-06-09 22:17:28 +0900

    add test for format

--- src/relative.test.ts
+++ src/relative.test.ts
@@ -2,6 +2,11 @@ import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 import { getRelativeDate, relative, relativeWith, getFromNow } from './relative'
 import { cdate } from 'cdate'
+import { before } from 'node:test'
+
+test.before(() => {
+  process.env.TZ = 'UTC'
+})
 
 // Today is Saturday
 const strToDate = (d: string) => new Date(Date.parse(d))
@@ -81,20 +86,84 @@ test('format returns correct when over last week past and over next week future'
   }
 })
 
+test('getFromNow returns correct', async () => {
+  const tests = [
+    // Future
+    { args: '2024-02-10T00:00:01.000Z', want: 'in 1 seconds', tz: 'UTC' },
+    { args: '2024-02-10T00:01:00.000Z', want: 'in 1 minutes', tz: 'UTC' },
+    { args: '2024-02-10T00:59:00.000Z', want: 'in 59 minutes', tz: 'UTC' },
+    { args: '2024-02-10T01:00:00.000Z', want: 'in 1 hours', tz: 'UTC' },
+    { args: '2024-02-10T23:00:00.000Z', want: 'in 23 hours', tz: 'UTC' },
+    { args: '2024-02-11T00:00:00.000Z', want: 'in 1 days', tz: 'UTC' },
+    { args: '2024-03-10T23:59:59.000Z', want: 'in 29 days', tz: 'UTC' },
+    { args: '2024-03-12T00:00:00.000Z', want: 'in 1 months', tz: 'UTC' },
+    { args: '2025-02-08T23:59:59.000Z', want: 'in 12 months', tz: 'UTC' },
+    { args: '2025-02-10T00:00:00.000Z', want: 'in 1 years', tz: 'UTC' },
+    { args: '2048-02-10T00:00:00.000Z', want: 'in 24 years', tz: 'UTC' },
+    { args: '2048-02-10T00:00:00.000Z', want: 'in 24 years', tz: 'Asia/Tokyo' },
+    // Past
+    { args: '2024-02-09T23:59:59.000Z', want: '1 seconds ago', tz: 'UTC' },
+    { args: '2024-02-09T23:59:59.000Z', want: '1 seconds ago', tz: 'Asia/Tokyo' },
+    { args: '2024-02-09T23:59:00.000Z', want: '1 minutes ago', tz: 'UTC' },
+    { args: '2024-02-09T23:00:01.000Z', want: '59 minutes ago', tz: 'UTC' },
+    { args: '2024-02-09T23:00:00.000Z', want: '1 hours ago', tz: 'UTC' },
+    { args: '2024-02-09T00:00:01.000Z', want: '23 hours ago', tz: 'UTC' },
+    { args: '2024-02-09T00:00:00.000Z', want: '1 days ago', tz: 'UTC' },
+    { args: '2024-01-11T00:00:01.000Z', want: '29 days ago', tz: 'UTC' },
+    { args: '2024-01-11T00:00:00.000Z', want: '1 months ago', tz: 'UTC' },
+    { args: '2023-02-10T00:00:01.000Z', want: '12 months ago', tz: 'UTC' },
+    { args: '2023-02-10T00:00:00.000Z', want: '1 years ago', tz: 'UTC' },
+    { args: '2000-02-10T00:00:00.000Z', want: '24 years ago', tz: 'UTC' },
+  ]
+
+  for (const t of tests) {
+    const { args, want, tz } = t
+    process.env.TZ = tz
+    const got = getFromNow(strToDate(args), { specificNow: todayDate })
+    assert.equal(got, want)
+  }
+})
+
 test('handler is works as cdate handler', async () => {
+  const tests = [
+    { args: 'R H:mm A', want: 'February 10, 2023 0:00 AM' },
+    { args: 'F', want: `${new Date(Date.now()).getFullYear() - 2023} years ago` },
+  ]
   const target = '2023-02-10T00:00:00.000Z'
   const cal = cdate().handler(relative).cdateFn()
-  const got = cal(target).format('R H:mm A')
-  const want = 'February 10, 2023 9:00 AM'
-  assert.equal(got, want)
+
+  for (const t of tests) {
+    const { args, want } = t
+    const got = cal(target).format(args)
+    assert.equal(got, want)
+  }
 })
 
 test('handler with locale is works as cdate handler', async () => {
   const target = '2023-02-10T00:00:00.000Z'
-  const cal = cdate().handler(relativeWith({ others: 'YYYY年M月DD日' })).cdateFn()
-  const got = cal(target).locale('ja').format('R AH:mm')
-  const want = '2023年2月10日 午前9:00'
-  assert.equal(got, want)
+  const labels = {
+    others: 'YYYY年M月DD日',
+    future: '%s後',
+    past: '%s前',
+    seconds: '秒',
+    minutes: '分',
+    hours: '時間',
+    days: '日',
+    months: 'ヶ月',
+    years: '年',
+  }
+  const cal = cdate().handler(relativeWith(labels)).cdateFn()
+
+  const tests = [
+    { args: 'R AH:mm', want: '2023年2月10日 午前0:00' },
+    { args: 'F', want: `${new Date(Date.now()).getFullYear() - 2023} 年前` },
+  ]
+
+  for (const t of tests) {
+    const { args, want } = t
+    const got = cal(target).locale('ja').format(args)
+    assert.equal(got, want)
+  }
 })
 
 test.run()
